4ef594dfb1a8f24bb7e15c230b820cf4efcbee0c
Vladislav Ivanishin <vlad@ispras.ru>
2016-09-06 11:39:16 +0300

    Git pull cron job: do hard reset (desired if e.g. an amend happened)

--- shell/crontab/git-pull-gcc-from-titan.sh
+++ shell/crontab/git-pull-gcc-from-titan.sh
@@ -1,11 +1,18 @@
 #! /bin/sh -
 
-logfile=/media/big/cron-logs/git-pull-gcc.log
+logfile="/media/big/cron-logs/git-pull-gcc.log"
 exec 2>>$logfile
 exec 1>>$logfile
 date_begin=$(date)
 
 echo "=========== $date_begin BEGIN ==========="
+echo
+
 cd /home/vlad/gcc-gomp
-git pull
+git fetch --all
+git reset --hard origin/elf-semantics-for-nvptx
+saved=$?
+[ $saved -eq 0 ] || echo "Error code $saved"
+
+echo
 echo "============ $date_begin END ($(date)) ============"
