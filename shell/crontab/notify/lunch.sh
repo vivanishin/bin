3d142e396bc76f37595be0de4823c7478b0c34c6
Vladislav Ivanishin <vlad@ispras.ru>
2016-09-28 16:27:52 +0300

    Manage my crons!

--- shell/crontab/notify/lunch.sh
+++ shell/crontab/notify/lunch.sh
@@ -1,3 +1,5 @@
 #! /bin/sh -
 
-DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/$(id -u) yad --title="My Master,"  --text="LUNCH time!" --text-align=center --center
+event=${1:-LUNCH}
+
+DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/$(id -u) yad --title="My Master,"  --text="$event time!" --text-align=center --center
--- shell/crontab/notify/seminar.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh -
-
-DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/$(id -u) yad --title="My Master,"  --text="Seminar" --text-align=center --center
