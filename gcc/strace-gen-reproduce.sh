ecdf46072fa14c02c39ac620467bc6e81ab6477f
Vladislav Ivanishin <vlad@ispras.ru>
2017-02-14 13:41:59 +0300

    strace-gen-reproduce: Enable explicit pid spec
    * gcc/strace-gen-reproduce.sh: User can now explicitly specify the
    desired pid whose invocation to reproduce.
    * gcc/strace-treeify: Comment/uncomment the awk line to get more info on
    the invocation (all the arguments).

--- gcc/strace-gen-reproduce.sh
+++ gcc/strace-gen-reproduce.sh
@@ -5,14 +5,16 @@ usage_and_exit()
   cat <<- EOF
 	Usage:
 
-		$0 <strace-log-filename> [<repro-script-filename>]
+		$0 <strace-log-filename> [<repro-script-filename> [<pid>]]
 
 	The first argument is an strace log obtained with strace -v (for environment
 	variables). It creates a script (named as the second argument if specified,
 	otherwise 'reproduce.sh') in the current directory.
 
 	The generated script sets up the environment and invokes the first program
-	whose invocation failed in the strace log with the same arguments.
+	whose invocation failed in the strace log with the same arguments. If <pid>
+	is provided, that process's invocation is reproduced (instead of the failing
+	process's).
 
 	The failing program is started in a GDB session. To override this, specify
 	GGA environment variable e.g.:
@@ -38,8 +40,15 @@ newline "$repro_script"
 echo "# !! generated with $0 !!" >> "$repro_script"
 newline "$repro_script"
 
-fail_pid_first=$(cat "$strace_log" | grep 'exited with' | grep -v 'exited with 0' | head -1 | awk '{print $1}')
-grep "$fail_pid_first" "$strace_log" | strace-exports.sh >> "$repro_script"
+if [ -n "$3" ]
+then
+  repro_pid="$3"
+else
+  fail_pid_first=$(cat "$strace_log" | grep 'exited with' | grep -v 'exited with 0' | head -1 | awk '{print $1}')
+  repro_pid=$fail_pid_first
+fi
+
+grep "$repro_pid" "$strace_log" | strace-exports.sh >> "$repro_script"
 
 newline "$repro_script"
 echo 'source common.sh' >> "$repro_script"
@@ -47,7 +56,7 @@ echo 'GA="gdb --args"' >> "$repro_script"
 newline "$repro_script"
 
 printf '${GGA-$GA} ' >> "$repro_script"
-grep "$fail_pid_first" "$strace_log" |
+grep "$repro_pid" "$strace_log" |
   grep execve |
     sed -e 's;execve;;g' |
       grep -v resumed |
