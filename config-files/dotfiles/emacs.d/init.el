05acf8becd47e412d38dff9f569111a5703cc635
Vladislav Ivanishin <vladislav.ivanishin@gmail.com>
2017-03-05 00:44:10 +0300

    emacs: package auto-generated magic

--- config-files/dotfiles/emacs.d/init.el
+++ config-files/dotfiles/emacs.d/init.el
@@ -109,7 +109,13 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(package-selected-packages (quote (solarized-theme evil)))
+ '(package-archives
+   (quote
+    (("melpa" . "http://melpa.org/packages/")
+     ("gnu" . "http://elpa.gnu.org/packages/")
+     ("marmalade" . "http://marmalade-repo.org/packages/")
+     ("org" . "http://orgmode.org/elpa/"))))
+ '(package-selected-packages (quote (evil-magit magit use-package solarized-theme evil)))
  '(scheme-program-name "guile")
  '(scroll-bar-mode nil)
  '(show-paren-mode t))
