90e602ebda4a0ed934e0e2c8e3df6daeb0a727c6
Vladislav Ivanishin <vladislav.ivanishin@gmail.com>
2017-05-24 13:37:39 +0300

    Make the notifier script slightly more flexible
    modified:   shell/crontab/notify/lunch.sh

--- shell/crontab/notify/lunch.sh
+++ shell/crontab/notify/lunch.sh
@@ -1,5 +1,6 @@
 #! /bin/sh -
 
 event=${1:-LUNCH}
+suffix="${2-time!}"
 
-DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/$(id -u) yad --title="My Master,"  --text="$event time!" --text-align=center --center
+DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/$(id -u) yad --title="My Master,"  --text="$event $suffix" --text-align=center --center
