754f40ffb65621115f4001cbcd3818b1c5a971e4
Vladislav Ivanishin <vladislav.ivanishin@gmail.com>
2018-08-03 10:00:17 +0300

    kindle-get-clippings: update for arch

--- kindle-get-clippings.sh
+++ kindle-get-clippings.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -
 
-from='/run/media/vlad/Kindle/documents/My Clippings.txt'
-to="my-clippings-"$(date +%Y-%m-%d)".txt"
+from="/mnt/kindle/documents/My Clippings.txt"
+to="/mnt/co/notes/personal/kindle-clippings.txt"
 
 if [ -f "$from" ]
 then
   cp "$from" "$to"
+  chmod -x "$to"
+  dos2unix "$to"
+  sed -i 's/ $//g' "$to"
 else
   echo "Cannot find '$from'. Check if the device is connected."
   exit 1
